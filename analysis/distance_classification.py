91e7f70f635079f6745192c56fec4f45240c65df
Elias Djurfeldt <eliasdjurfeldt@gmail.com>
2016-12-08 15:05:38 +0000

    Added conditional rounding of distances.

--- analysis/distance_classification.py
+++ analysis/distance_classification.py
@@ -1,10 +1,10 @@
 from util.data_io import load_csv, export_csv
 import numpy as np
 
-CENTRAL_LAS_VEGAS = [36.114679, -115.172834]  # Center of 'The Strip' as specified by Google Maps
+CENTRAL_LAS_VEGAS = [36.114679, -115.172834]  # Centre of 'The Strip' as specified by Google Maps
 
 
-def calculate_distance(lat, long):
+def calculate_distance(lat, long, rounded):
     """
     Distance is defined as the euclidean distance between point (x,y) and (a,b).
     This is sufficient for this purpose as I am only looking at one city and do not need to take into account e.g. earth's curvature
@@ -14,23 +14,25 @@ def calculate_distance(lat, long):
     :param long: longitude of restaurant
     :return: the absolute Pythagoras distance from "CENTRAL_LAS_VEGAS" as defined above
     """
-
-    distance = round(np.sqrt(abs(lat - CENTRAL_LAS_VEGAS[0]) ** 2 + abs(long - CENTRAL_LAS_VEGAS[1]) ** 2) * 100, 0)
+    if rounded:
+        distance = round(np.sqrt(abs(lat - CENTRAL_LAS_VEGAS[0]) ** 2 + abs(long - CENTRAL_LAS_VEGAS[1]) ** 2) * 100, 0)
+    else:
+        distance = np.sqrt(abs(lat - CENTRAL_LAS_VEGAS[0]) ** 2 + abs(long - CENTRAL_LAS_VEGAS[1]) ** 2) * 100
     return distance
 
 
-def add_distance_col(df):
+def add_distance_col(df, rounded=None):
     """
     Aerial distance from "CENTRAL_LAS_VEGAS" is added as a column to the dataframe
     :param df: pandas dataframe that contains the filtered yelp data
     :return: new df with the added column for distance from "CENTRAL_LAS_VEGAS"
     """
 
-    df['distance_from_central'] = calculate_distance(df['latitude'], df['longitude'])
+    df['distance_from_central'] = calculate_distance(df['latitude'], df['longitude'], rounded)
     return df
 
 
 if __name__ == '__main__':
     df = load_csv("relevant_cols_only_restaurants.csv")
-    df = add_distance_col(df)
+    df = add_distance_col(df, True)
     export_csv(df, "distance_col_yelp_business.csv")
